95b0396742c265f9f3b296507ad055e958c2e339
ghoshsreyoshi <ghosh.sr@northeastern.edu>
2022-04-17 15:29:28 -0400

    format changed

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -6,19 +6,18 @@ import InternetRadio from './Components/InternetRadio/InternetRadio';
 import { Route, BrowserRouter as Router, Link, Routes } from 'react-router-dom'
 
 function App() {
-    return (
-        <div className="App">
-            <div>
-                <Router>
-                    <Routes>
-                        <Route path='/MusicPlaying' element={< MusicPlaying />} > < /Route>
-                            <Route path='/AccountSetting' element={< AccountSetting />} > < /Route>
-                                <Route path='/InternetRadio' element={< InternetRadio />} > < /Route>
+  return (
+    <div className="App">
+      <div>
+        <Router>
+          <Routes>
+            <Route path='/MusicPlaying' element={< MusicPlaying />}> </Route>
+            <Route path='/AccountSetting' element={< AccountSetting />}> </Route>
+            <Route path='/InternetRadio' element={< InternetRadio />}> </Route>
+          </Routes> </Router> </div>
 
-                                </Routes> </Router > </div>
-
-                    </div>
-                    );
+    </div>
+  );
 }
 
-                    export default App;
+export default App;
