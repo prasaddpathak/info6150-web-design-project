b33c21234c62db05e31af4b008ee8476ddee2eb5
Trisha Ghorpade <ghorpade.t@northeastern.edu>
2022-04-22 02:27:30 -0400

    music api and updated filenames

--- server/src/routes/user-router.js
+++ server/src/routes/user-router.js
@@ -1,5 +1,6 @@
 import express from "express";
 import * as userController from './../controllers/user-controller.js';
+import * as musicController from './../controllers/music-controller.js';
 
 const router = express.Router();
 
@@ -14,5 +15,9 @@ router.route('/user/:id')
     .get(userController.get)
     .put(userController.update)
     .delete(userController.remove);
+
+router.route('/music')
+    .post(musicController.postallMusic)
+    .get(musicController.allMusic);    
 
 export default router;
