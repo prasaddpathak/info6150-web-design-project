8073294b20c29abd5381fbe74721c599dc89d628
Prasad Pathak <pathak.pra@northeastern.edu>
2022-04-23 15:52:23 -0400

    Updated Navbar / Added Delete Option for Playlists

--- webapp/src/Components/NavBar/NavBarOptions/NavBarOptions.jsx
+++ webapp/src/Components/NavBar/NavBarOptions/NavBarOptions.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-import {Button} from "@material-ui/core";
+import './NavBarOptions.scss'
+// import {Button} from "@material-ui/core";
 import {Link} from "react-router-dom";
+import {Button, IconButton} from '@material-ui/core';
+import {Delete} from "@material-ui/icons";
+
+
 
 class SideBarOptions extends React.Component {
 
@@ -9,20 +14,41 @@ class SideBarOptions extends React.Component {
         super(props);
         this.myRef = React.createRef();
         this.state = {  
+            title : this.props.title
         };
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    handleDelete() {
+        alert(this.state.title);
     }
 
     render() {
         const Icon = this.props.Icon;
         const title = this.props.title;
         const className = this.props.className;
+        const isPlaylistItem = this.props.isPlaylistItem;
         // const sideBarRef = useRef();
         const href = this.props.href;
+        const playlistClass = isPlaylistItem? "playlistItem" : "";
         return (
-            <Button onClick={()=>{this.myRef.current.click();window.location.reload();}} className={className} startIcon={Icon && <Icon/>}>
-                <Link ref={this.myRef} to={href}/>
-                {title}
-            </Button>
+            <div className={playlistClass}>
+                <Button onClick={()=>{this.myRef.current.click();window.location.reload();}} className={className} startIcon={Icon && <Icon/>}>
+                    <Link ref={this.myRef} to={href}/>
+                    {title}
+                </Button>
+                {
+                    isPlaylistItem ? 
+                        <IconButton color="secondary" onClick={this.handleDelete}>
+                            <Delete></Delete>
+                        </IconButton>
+                    :
+                    <></>
+                }  
+                
+                
+            </div>
+            
         );
     }
 }
