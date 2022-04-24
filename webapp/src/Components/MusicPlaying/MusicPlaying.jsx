7dea831f641b148c5bd4d4a41bd1f85dd6de7b1f
ghoshsreyoshi <ghosh.sr@northeastern.edu>
2022-04-24 17:10:35 -0400

    documentations added

---
+++ webapp/src/Components/MusicPlaying/MusicPlaying.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Song from '../../Assets/Audio/song.mp3';
+import songImg from '../../Assets/Img/songImage.jpg';
+import { Link } from 'react-router-dom';
+import './MusicPlaying.scss'
+import song from '../../Assets/Audio/song.mp3'
+import disc from '../../Assets/Img/songDisk.png'
+
+//class musicplaying component defined
+
+class MusicPlaying extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            start: false
+        }
+    }
+
+    startSong = () => {
+        this.setState({ start: true })
+    }
+    pauseSong = () => {
+        this.setState({ start: false })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className='musicMain'>
+                    <div className='row'>
+                        <div className='internetRadio col-md-10 offset-md-2s' >
+                            <div className="box"><div className={this.state.start ? "diskStart" : 'diskStop'}></div></div>
+                            <div>
+                                <img className='songImgMusic' src={songImg} />
+                            </div>
+                            <div className='audioDiv' >
+                                <audio
+                                    className='audio'
+                                    controls
+                                    onPlay={this.startSong}
+                                    onPause={this.pauseSong}
+                                    // src='http://youtu.be/qJT0mc3q6Lg'>
+                                    src={song}>
+                                    <code>audio</code> element.
+                                </audio>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default MusicPlaying
