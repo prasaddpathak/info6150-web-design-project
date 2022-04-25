66470bd3a380bbcea034f9ab45f07712261476f4
Trisha Ghorpade <ghorpade.t@northeastern.edu>
2022-04-25 08:03:33 -0400

    image added to music card

---
+++ webapp/src/Components/Header/DropDownGenreList/LangList/LangList.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './LangList.scss';
+import Button from "@material-ui/core/Button";
+import {Link} from "react-router-dom";
+
+class LangList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isSelected : false,
+            onSelect : {}
+        };
+        this.handleSelected = this.handleSelected.bind(this);
+    }
+
+    handleSelected = e => {
+        e.stopPropagation();
+        this.props.onClick(this.props.item,!this.state.isSelected);
+        this.setState({
+            isSelected : !this.state.isSelected
+        });
+        if (this.state.isSelected) {
+            this.setState({
+                onSelect : {}
+            });
+        } else {
+            this.setState({
+                onSelect : {
+                    backgroundColor:"#a2d5f2",
+                    color:"#191919"
+                }
+            });
+        }
+    };
+
+    render() {
+        return (
+            <Button style={this.state.onSelect} onClick={this.handleSelected}>
+                <Link to={"/home/events"}/>
+                {this.props.item}
+            </Button>
+    )};
+}
+
+export default LangList;
--- webapp/src/Components/MusicContainer/Card/Card.jsx
+++ webapp/src/Components/MusicContainer/Card/Card.jsx
@@ -12,12 +12,11 @@ import { increaseTimesPlayed, setCurrentPlaying } from "../../../Store/Actions/m
 import SongName from "../SongNames/SongName";
 import { Skeleton } from "@material-ui/lab";
 import Box from "@material-ui/core/Box";
-import first from '../../MusicImg/musicpicture.jpg';
-import { Link } from 'react-router-dom';
-import { useHistory } from "react-router-dom";
+import first from '../../../Assets/Img/Beach.jpg';
+
 function MusicCard(props) {
     const { name, img, author_name } = props.music;
-    const history = useHistory();
+
     const [isHovered, setHovered] = useState(false);
 
     function handleResponse() {
@@ -36,12 +35,6 @@ function MusicCard(props) {
         dispatch(setCurrentPlaying(props.music))
         dispatch(increaseTimesPlayed(props.music.id));
     }
-    function selectSong() {
-        sessionStorage.setItem('songName', name)
-        history.push("/home/playMusic");
-        window.location.reload();
-    }
-
 
     return (
         <div className={"music-card"}>
@@ -56,11 +49,26 @@ function MusicCard(props) {
                     </div>
                     :
                     <>
-
                         <div onClick={handlePlay} className={"music-card-cover"} onMouseOver={handleResponse}>
-                            <img src={first} alt={name} />
-                            <div hr className="play-circle">
-                                <a onClick={selectSong}> <PlayCircleFilledWhiteIcon /></a>
+                            {/* {console.log("img:"+img)}
+                            {console.log("name:"+name)} */}
+
+                            {/* Below logic is for 
+                             - paylist img src : "http://localhost:3000/"+img" 
+                             - else condition to handle empty img details*/}
+                            {
+                            window.location.pathname.includes("home/playlist") ?
+                            console.log("get img name: "+img) : console.log("not in playlist")
+                            }
+                            {
+                                window.location.pathname.includes("home/playlist") ?
+                                img ? <img src={"http://localhost:3000/"+img} alt={name} /> : <img src={first} alt={name} />
+                                :
+                                img ? <img src={img} alt={name} /> : <img src={first} alt={name} />
+                                
+                            } 
+                            <div className="play-circle">
+                                <PlayCircleFilledWhiteIcon />
                             </div>
                         </div>
                         <React.Fragment>
@@ -71,7 +79,7 @@ function MusicCard(props) {
             }
 
 
-        </div >
+        </div>
     );
 }
 
