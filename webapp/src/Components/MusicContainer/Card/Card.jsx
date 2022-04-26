5fc9b91f0ecf9bd4f1a2f99cdad0a52215fc7eba
Gloria Singh <singh.gl@northeastern.edu>
2022-04-26 18:47:55 -0400

    few correction

--- webapp/src/Components/MusicContainer/Card/Card.jsx
+++ webapp/src/Components/MusicContainer/Card/Card.jsx
@@ -12,11 +12,18 @@ import { increaseTimesPlayed, setCurrentPlaying } from "../../../Store/Actions/m
 import SongName from "../SongNames/SongName";
 import { Skeleton } from "@material-ui/lab";
 import Box from "@material-ui/core/Box";
-import first from '../../../Assets/Img/Beach.jpg';
+
+// import first from '../../MusicImg/musicpicture.jpg';
+import { Link } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
+// =======
+ import first from '../../../Assets/Img/Beach.jpg';
+
+
 
 function MusicCard(props) {
     const { name, img, author_name } = props.music;
-
+    const history = useHistory();
     const [isHovered, setHovered] = useState(false);
 
     function handleResponse() {
@@ -36,6 +43,14 @@ function MusicCard(props) {
         dispatch(increaseTimesPlayed(props.music.id));
     }
 
+    function selectSong() {
+        sessionStorage.setItem('songName', name)
+        history.push("/home/playMusic");
+        window.location.reload();
+    }
+
+
+
     return (
         <div className={"music-card"}>
             {
@@ -49,7 +64,15 @@ function MusicCard(props) {
                     </div>
                     :
                     <>
+
                         <div onClick={handlePlay} className={"music-card-cover"} onMouseOver={handleResponse}>
+
+                            <img src={first} alt={name} />
+                            <div hr className="play-circle">
+                                <a onClick={selectSong}> <PlayCircleFilledWhiteIcon /></a>
+                            </div>
+
+
                             {/* {console.log("img:"+img)}
                             {console.log("name:"+name)} */}
 
@@ -69,6 +92,8 @@ function MusicCard(props) {
                             } 
                             <div className="play-circle">
                                 <PlayCircleFilledWhiteIcon />
+
+
                             </div>
                         </div>
                         <React.Fragment>
@@ -79,7 +104,7 @@ function MusicCard(props) {
             }
 
 
-        </div>
+        </div >
     );
 }
 
